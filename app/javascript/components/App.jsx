d04b6171ec14d6d0fbac6d5a0cc83d134c31284a
Seow Alex <seowalex@hotmail.com>
2019-12-09 18:35:59 +0800

    Add ESLint and fix files

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,11 +1,10 @@
-import React from "react"
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
-} from "react-router-dom"
-import Home from "../components/Home"
+} from 'react-router-dom';
+import Home from './Home';
 
 export default function App() {
   return (
@@ -14,5 +13,5 @@ export default function App() {
         <Route path="/" exact component={Home} />
       </Switch>
     </Router>
-  )
+  );
 }
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,18 +1,16 @@
-import React from "react"
+import React from 'react';
 
-const Home = props => {
-  return (
-    <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-      <div className="jumbotron jumbotron-fluid bg-transparent">
-        <div className="container secondary-color">
-          <h1 className="display-4">Food Recipes</h1>
-          <p className="lead">
-            A curated list of recipes for the best homemade meal and delicacies.
-          </p>
-        </div>
+const Home = () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Food Recipes</h1>
+        <p className="lead">
+          A curated list of recipes for the best homemade meal and delicacies.
+        </p>
       </div>
     </div>
-  );
-}
+  </div>
+);
 
-export default Home
+export default Home;
