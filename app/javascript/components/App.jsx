147cbf7c0518b381ecee130e75a0ef22ebe7ed7a
Seow Alex <seowalex@hotmail.com>
2019-12-09 17:51:47 +0800

    Clean up app structure

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -7,16 +7,12 @@ import {
 } from "react-router-dom"
 import Home from "../components/Home"
 
-class App extends React.Component {
-  render () {
-    return (
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
-      </Router>
-    );
-  }
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  )
 }
-
-export default App
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,21 +1,18 @@
 import React from "react"
-import PropTypes from "prop-types"
 
-class Home extends React.Component {
-  render () {
-    return (
-      <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-        <div className="jumbotron jumbotron-fluid bg-transparent">
-          <div className="container secondary-color">
-            <h1 className="display-4">Food Recipes</h1>
-            <p className="lead">
-              A curated list of recipes for the best homemade meal and delicacies.
-            </p>
-          </div>
+const Home = props => {
+  return (
+    <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+      <div className="jumbotron jumbotron-fluid bg-transparent">
+        <div className="container secondary-color">
+          <h1 className="display-4">Food Recipes</h1>
+          <p className="lead">
+            A curated list of recipes for the best homemade meal and delicacies.
+          </p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Home
