782e20a7b444dcc5bc8615e9451fc8a8d2a21bb7
Seow Alex <seowalex@hotmail.com>
2019-12-10 10:43:37 +0800

    Add page titles

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const Home = () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Home</h1>
-        <p className="lead">
-          A curated list of recipes for the best homemade meal and delicacies.
-        </p>
+  <>
+    <Helmet>
+      <title>Todo Manager</title>
+    </Helmet>
+    <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+      <div className="jumbotron jumbotron-fluid bg-transparent">
+        <div className="container secondary-color">
+          <h1 className="display-4">Home</h1>
+          <p className="lead">
+            A curated list of recipes for the best homemade meal and delicacies.
+          </p>
+        </div>
       </div>
     </div>
-  </div>
+  </>
 );
 
 export default Home;
--- app/javascript/components/NotFound.jsx
+++ app/javascript/components/NotFound.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const Error = () => (
-  <div className="vw-100 vh-100 d-flex align-items-center justify-content-center">
-    <div className="jumbotron bg-transparent">
-      <h1 className="display-4">404 Not Found</h1>
-      <p className="lead">
-        Sorry, an error has occured. Requested page not found!
-      </p>
+  <>
+    <Helmet>
+      <title>404 Not Found</title>
+    </Helmet>
+    <div className="vw-100 vh-100 d-flex align-items-center justify-content-center">
+      <div className="jumbotron bg-transparent text-center">
+        <h1 className="display-4">404 Not Found</h1>
+        <p className="lead">
+          The page you were looking for doesn&apos;t exist.
+          <br />
+          You may have mistyped the address or the page may have moved.
+        </p>
+      </div>
     </div>
-  </div>
+  </>
 );
 
 export default Error;
