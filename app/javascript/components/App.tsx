bcf0ef4a2f5de316af9515ec554440f79c1b10fb
Seow Alex <seowalex@hotmail.com>
2019-12-28 13:41:28 +0800

    Use function expression instead of function declaration

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -7,17 +7,17 @@ import {
 import Home from 'components/Home';
 import NotFound from 'components/NotFound';
 
-export default function App(): JSX.Element {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
-  );
-}
+const App: React.FunctionComponent = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
+  </Router>
+);
+
+export default App;
