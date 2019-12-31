17b6e271febc43fa4cfbcb9f62ade10e8d23bd1a
Seow Alex <seowalex@hotmail.com>
2019-12-31 14:21:32 +0800

    Change to ApiController

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,4 +1,4 @@
-class Api::TasksController < ApplicationController
+class Api::TasksController < ApiController
   def task_params
     params.require(:task).permit(:title, :description, :completed, :priority, :position, :due_date, :tag_list)
   end
