b82422319878508683fb87b96de194d55d45102a
Seow Alex <seowalex@hotmail.com>
2019-12-31 15:02:12 +0800

    Add seed data

--- app/resources/api/task_resource.rb
+++ app/resources/api/task_resource.rb
@@ -1,3 +1,3 @@
 class Api::TaskResource < JSONAPI::Resource
-  attributes :title, :description, :completed, :priority, :position, :due_date
+  attributes :title, :description, :completed, :priority, :position, :due_date, :tag_list
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: "Lorem Ipsum", description: "This is a task!", completed: false, priority: 1, position: 1, due_date: Date.new(2019, 1, 1), tag_list: ["awesome", "slick", "hefty"])
+Task.create(title: "Hello World", description: "This is another task!", completed: false, priority: 3, position: 2, due_date: Date.new(2019, 1, 2), tag_list: ["awesome"], ["slick"], ["light"])
