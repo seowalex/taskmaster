1d1809b56f1103fe2b7e2f1ef7e1ad444d23d27c
Seow Alex <seowalex@hotmail.com>
2020-01-01 14:00:17 +0800

    Add authentication

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,8 @@
 class ApiController < JSONAPI::ResourceController
+  before_action :authenticate_user!
   protect_from_forgery with: :null_session
+
+  def index
+    process_request
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             },
+             defaults: { format: :json }
+
   namespace :api do
     jsonapi_resources :tasks
   end
