1e5e34a92c3af755e7d6ef62bec759b04ae5869f
Seow Alex <seowalex@hotmail.com>
2020-01-04 12:43:02 +0800

    Move components into individual folders

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,7 @@
 const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
 const typescript = require('./loaders/typescript');
+const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
@@ -9,5 +10,13 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
 }));
 
 environment.loaders.prepend('typescript', typescript);
+
+environment.config.merge({
+  resolve: {
+    plugins: [
+      new DirectoryNamedWebpackPlugin(true),
+    ],
+  },
+});
 
 module.exports = environment;
