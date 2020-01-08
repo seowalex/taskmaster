da728fc7f60aea4c397cc4104ddda1641eb8a13a
Seow Alex <seowalex@hotmail.com>
2020-01-08 15:18:56 +0800

    Fix tag search

--- app/resources/api/task_resource.rb
+++ app/resources/api/task_resource.rb
@@ -2,9 +2,8 @@ class Api::TaskResource < JSONAPI::Resource
   attributes :title, :description, :completed, :priority, :position, :due_date, :tag_list
 
   filter :search, apply: ->(records, value, _options) {
-    # TODO: Use regex and filter out into function
     search = value.join(",").split(" ").select{ |word| word[0] != "#" }.join(" ")
-    tags = value.join(",").scan(/(?:(?<=^#)|(?<=\s#))\w+(?=$|\s)/)
+    tags = value.join(",").scan(/(?:(?<=^#)|(?<=\s#))[^\s]+/)
 
     if tags.any?
       records.where("title ilike ?", "%#{search}%").tagged_with(tags, wild: true)
